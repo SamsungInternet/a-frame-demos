d1c3320d1c2c5068bd8a5923897e8f6e36b46a7a
Ada Rose Edwards <ada@ada.is>
2016-08-29 12:32:13 +0100

    Reorganise repo

--- scripts/init-service-worker.js
+++ scripts/init-service-worker.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('sw.js', { scope: '/' })
+	navigator.serviceWorker.register('sw.js')
 	.then(function(reg) {
 		console.log('sw registered', reg);
 	}).catch(function(error) {
--- sw.js
+++
@@ -1,8 +0,0 @@
-/* global toolbox, importScripts, self */
-/* jshint browser:true */
-'use strict';
-
-importScripts('scripts/sw-toolbox.js');
-
-toolbox.options.networkTimeoutSeconds = 3;
-toolbox.router.default = (location.protocol === 'http:' || location.hostname === 'localhost') ? toolbox.networkFirst : toolbox.fastest;
