4936a6490f9e12b96f07bc5f0ec367d2d48fb8c5
Peter O'Shaughnessy <peter.oshaughnessy@gmail.com>
2017-01-31 12:53:15 +0000

    Starting to add speech recognition

--- speech/js/demo.js
+++ speech/js/demo.js
@@ -1,7 +1,47 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+var recognising = false;
+var recognition = new SpeechRecognition();
+
+recognition.continuous = true;
+recognition.interimResults = false;
+
+recognition.onstart = function() {
+  recognising = true;
+  // TODO show recording is on?
+};
+
+recognition.onerror = function(event) {
+  console.error('Speech recognition error', event);
+};
+  
+recognition.onend = function() {
+  recognising = false;  
+};
+  
+recognition.onresult = function(event) {
+  console.log('On result', event.results);  
+};
+
 function startListening() {
-  alert('start listening');
+
+  var recordButton = document.getElementById('btn-record');
+
+  console.log('recordButton', recordButton.object3D);
+
+  if (recognising) {
+    recognition.stop();
+    // TODO change button text
+
+  } else {
+    recognition.start();
+    // TODO change button text
+  }
+
 }
 
 function sendTweet() {
-  alert('send tweet');
+  console.log('TODO send tweet');
 }
